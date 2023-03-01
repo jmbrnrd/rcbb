39b7a15f6947332e24c42c608f3ae6d45216b20f
James Barnard <jmbarnard@gmail.com>
2023-03-01 10:33:17 +0000

    init

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 module.exports = function (config) {
   config.addWatchTarget("./src/assets/sass/");
   config.addShortcode("year", () => `${new Date().getFullYear()}`);
-  config.addWatchTarget('./src/assets/scripts/')
+  config.addWatchTarget('./src/assets/scripts/');
+  config.addPassthroughCopy("./src/assets/fonts/");
+  config.addPassthroughCopy("./src/assets/imgs/");
   return {
     //templateFormats: ['md', '11ty.js'],
     dir: {
