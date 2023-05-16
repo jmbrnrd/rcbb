c39ac790e9bb7dbb81f6577cd0ba54f90d8cc1ba
James Barnard <jmbarnard@gmail.com>
2023-05-16 17:08:23 +0100

    WIP TRANSFER

--- src/assets/scripts/nav.js
+++ src/assets/scripts/nav.js
@@ -36,6 +36,6 @@ export default function() {
                 ? header.classList.remove('active')
                 : header.classList.add('active');
         });
-    }, { rootMargin: '-150px'} );
-    observer.observe(document.getElementById('top'));
+    }, { rootMargin: '50px'} );
+   observer.observe(document.getElementById('scroll-watcher'));
 }
