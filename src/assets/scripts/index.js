6f09011d1a22c66916ae998ea407180268e597fd
James Barnard <jmbarnard@gmail.com>
2024-02-27 10:51:45 +0000

    Random hero etc.

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -4,3 +4,18 @@ import glightbox from './glightbox';
 
 scroll();
 glightbox();
+
+
+/**
+ * Randomly set the hero images
+ */
+function setHeroImage() {
+
+    const heroImages = ["IMG_8292.jpg","IMG_0001.jpg","IMG_8286.jpg"];
+    const hero = document.querySelector('.hero .bg')
+
+    hero.src =
+        'https://res.cloudinary.com/rdl/image/upload/v1707550787/rcbb/images/' +
+        heroImages[Math.floor(Math.random() * heroImages.length)];
+}
+setHeroImage();
