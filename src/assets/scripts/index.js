8077a324d7e6f27188ab5ab812d9a094ae966657
James Barnard <jmbarnard@gmail.com>
2024-02-28 13:18:14 +0000

    Perf. updates

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -15,7 +15,7 @@ function setHeroImage() {
     const hero = document.querySelector('.hero .bg')
 
     hero.src =
-        'https://res.cloudinary.com/rdl/image/upload/v1707550787/rcbb/images/' +
+        'https://res.cloudinary.com/rdl/image/upload/c_scale,f_auto,w_1200/v1707550787/rcbb/images/' +
         heroImages[Math.floor(Math.random() * heroImages.length)];
 }
 setHeroImage();
